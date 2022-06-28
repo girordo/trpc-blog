a302522d97aeda99a952949df27fb415433ff186
girordo <tsgiroldo@gmail.com>
2022-06-28 14:44:00 -0300

    :truck: | Moving api to pages folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ export default withTRPC<AppRouter>({
   config({ ctx }) {
     const url = process.env.NEXT_PUBLIC_VERCEL_URL
       ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
-      : "http://localhost:3000";
+      : "http://localhost:3000/api/trpc";
 
     const links = [
       loggerLink(),

f176a7a9f4b1d7fa97d00ba734aaa630f85ec8b1
girordo <tsgiroldo@gmail.com>
2022-06-28 16:04:20 -0300

    :sparkles: | Introducing Register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+function RegisterPage() {
+  return (
+    <>
+      <form>
+        <Link href="/login"></Link>
+      </form>
+    </>
+  );
+}
+
+export default RegisterPage;

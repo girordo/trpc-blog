76ea41197fa5827f2f51480af7e791425cfa6525
girordo <tsgiroldo@gmail.com>
2022-06-30 18:31:12 -0300

    :zap: | Improving code

--- src/server/route/user.router.ts
+++ src/server/route/user.router.ts
@@ -2,38 +2,70 @@ import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import {
   createUserOutputSchema,
   createUserSchema,
+  requestOtpSchema,
 } from "../../schema/user.schema";
 import * as trpc from "@trpc/server";
 import { createRouter } from "../createRouter";
 
-export const userRouter = createRouter().mutation("register-user", {
-  input: createUserSchema,
-  async resolve({ ctx, input }) {
-    const { email, name } = input;
+export const userRouter = createRouter()
+  .mutation("register-user", {
+    input: createUserSchema,
+    async resolve({ ctx, input }) {
+      const { email, name } = input;
 
-    try {
-      const user = await ctx.prisma.user.create({
-        data: {
+      try {
+        const user = await ctx.prisma.user.create({
+          data: {
+            email,
+            name,
+          },
+        });
+
+        return user;
+      } catch (e) {
+        if (e instanceof PrismaClientKnownRequestError) {
+          if (e.code === "P2002") {
+            throw new trpc.TRPCError({
+              code: "CONFLICT",
+              message: "User already exists",
+            });
+          }
+        }
+
+        throw new trpc.TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Internal server error",
+        });
+      }
+    },
+  })
+  .mutation("request-otp", {
+    input: requestOtpSchema,
+    async resolve({ ctx, input }) {
+      const { email, redirect } = input;
+
+      const user = await ctx.prisma.user.findUnique({
+        where: {
           email,
-          name,
         },
       });
 
-      return user;
-    } catch (e) {
-      if (e instanceof PrismaClientKnownRequestError) {
-        if (e.code === "P2002") {
-          throw new trpc.TRPCError({
-            code: "CONFLICT",
-            message: "User already exists",
-          });
-        }
+      if (!user) {
+        throw new trpc.TRPCError({
+          code: "NOT_FOUND",
+          message: "User not found",
+        });
       }
 
-      throw new trpc.TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "Internal server error",
+      const token = await ctx.prisma.loginToken.create({
+        data: {
+          redirect,
+          user: {
+            connect: {
+              id: user.id,
+            },
+          },
+        },
       });
-    }
-  },
-});
+    },
+  });
